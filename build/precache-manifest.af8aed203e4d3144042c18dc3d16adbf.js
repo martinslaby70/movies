bb2aa70915bfa1526ef755b3066f04fe57bc2d04
Martin Slaby <martinslaby70@gmail.com>
2020-10-20 21:17:29 +0200

    build update

---
+++ build/precache-manifest.af8aed203e4d3144042c18dc3d16adbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb15d02f984696eaf840e5b7fc7a989c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb06c9aad61e13cef4e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3eda38d0b1d7528decfa",
+    "url": "/static/css/main.005cd34e.chunk.css"
+  },
+  {
+    "revision": "6fb06c9aad61e13cef4e",
+    "url": "/static/js/2.334dda0d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.334dda0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eda38d0b1d7528decfa",
+    "url": "/static/js/main.8e8bb251.chunk.js"
+  },
+  {
+    "revision": "2e71319991222b421a36",
+    "url": "/static/js/runtime-main.22e28c68.js"
+  }
+]);
